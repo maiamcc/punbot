28e291c4560c7663d17acc89eb0bf4c1dbaa25cb
Maia <maia.mcc@gmail.com>
2014-10-09 15:03:36 -0400

    local copy of cmudict

--- punbot.py
+++ punbot.py
@@ -7,6 +7,8 @@ import re
 import sys
 import zulip
 
+nltk.data.path.append('./nltk_data/')
+
 PUN_CHANCE = 1 # number between 0 and 1 -- probabiltiy of making a pun off a valid msg
 HELP_MSG = """Hi, I'm punbot, here for all of your annoying pun needs! (Well, actually, only a single, very specifiy annoying pun need. Sorry about that.) Here's how I work:\n\n
 
